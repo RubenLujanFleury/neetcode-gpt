e46bdcb32651cf0ff6bf131b5679e1ee2aaae70b
RubenLujanFleury <ruben1lujan@icloud.com>
2026-05-23 21:42:30 -0400

    normalization

---
+++ model/normalization.py
@@ -0,0 +1,35 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    def forward(self, x: NDArray[np.float64], gamma: NDArray[np.float64], beta: NDArray[np.float64]) -> NDArray[np.float64]:
+        # x: 1D feature vector
+        # gamma: 1D scale parameter (same length as x)
+        # beta: 1D shift parameter (same length as x)
+        # eps = 1e-5
+        # Normalize: x_hat = (x - mean) / sqrt(var + eps)
+        # Scale and shift: out = gamma * x_hat + beta
+        # return np.round(your_answer, 5)
+        pass
+
+        n = len(x)
+        xsum = 0
+        vsum = 0
+        epsilom = 10**(-5)
+
+        # Mean across features
+        for i in range(n):
+            xsum += x[i]
+
+        mean = xsum / n
+
+        # Variance across features
+        for i in range(n):
+            vsum += (x[i] - mean) ** 2
+
+        variance_squared = vsum / n
+
+        x_hat = ((x - mean)/math.sqrt(variance_squared + epsilom))* gamma + beta
+        
+        return np.round(x_hat, 5)
